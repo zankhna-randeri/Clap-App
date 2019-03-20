88b90be5461c3b1a3eba2e5b06033168616ba2a1
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-03-20 16:43:32 -0700

    Added support actionbar code

--- app/src/main/java/sjsu/zankhna/android/sensor/clapapp/MainActivity.java
+++ app/src/main/java/sjsu/zankhna/android/sensor/clapapp/MainActivity.java
@@ -2,12 +2,22 @@ package sjsu.zankhna.android.sensor.clapapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
     }
 }
